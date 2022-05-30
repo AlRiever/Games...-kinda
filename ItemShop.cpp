3c26acb978e0ad38abeb8289ed53bd1c0c0e7eea
AlRiever <104616195+AlRiever@users.noreply.github.com>
2022-05-30 11:39:07 -0700

    Update ItemShop.cpp

--- ItemShop.cpp
+++ ItemShop.cpp
@@ -1,9 +1,13 @@
+//  These programs are just for me to practice the small aspects of a video game
+//  If usefull for you, you are always welcomed to use my codes
+
+
 #include <iostream>
 #include <string>
 
 const int numitems = 5;  //Number of items available in the shop
 const std::string shopitems[numitems] = {"Armour", "Sword", "Horse", "Meds", "Food"}; //All the Shop Iteams available
-char buy, sure, viewInv, agreedToBuy;
+char buy, sure, viewInv, agreedToBuy, bother;
 
 int main(){
 	int itemNo = 0, bought = 0;
@@ -24,39 +28,55 @@ int main(){
 		}
 	}
 
-	std::cout << "\nWould you like to purchase anything from the shop? (y/n)\n";
-	std::cin >> buy;
+	while (true) {
+		std::cout << "\nWould you like to purchase anything from the shop? (y/n)\n";
+		std::cin >> buy;
 
-	if (buy == 'y') {
-		std::cout << "Input the serial number of the iteam you would like to purchase.\n";
-		std::cin >> itemNo;  //if buy == 'y' then, user inputs the serial number of the item they want to buy
-		itemNo -= 1;
+		if (buy == 'y') {
+			std::cout << "Input the serial number of the iteam you would like to purchase.\n";
+			std::cin >> itemNo;  //if buy == 'y' then, user inputs the serial number of the item they want to buy
+			itemNo -= 1;
 
-		std::cout << "Are you sure you want to purchase " << shopitems[itemNo] << " (y/n)" << std::endl;
-		std::cin >> sure;
-	}
-	if (buy == 'n') {
-		std::cout << "Thank you for visiting our shop. It was a pleasure to meet you.\n";
-		std::cout << "Good luck for your journey.\n";  //if buy == 'n' then, program terminates with a goodbye message
-	}
+			std::cout << "Are you sure you want to purchase " << shopitems[itemNo] << " (y/n)" << std::endl;
+			std::cin >> sure;
+		}
+		if (buy == 'n') {
+			std::cout << "Thank you for visiting our shop. It was a pleasure to meet you.\n";
+			std::cout << "Good luck for your journey.\n";  //if buy == 'n' then, program terminates with a goodbye message
+		}
 
-	if (sure == 'y') {
-		std::cout << "Thank you for your purchase.\n";
-		inventory[itemNo] += 1;
-		bought = 1;
-	}
-	else {
-		std::cout << "Welp thats okay i guess, See you soon adventurer\n";
+		if (sure == 'y') {
+			std::cout << "Thank you for your purchase.\n";
+			inventory[itemNo] += 1;
+			bought = 1;
+		}
+		if (sure == 'n') {
+			std::cout << "Bother, to buy something else? (y/n)\n";
+			std::cin >> bother;
+		}
+
+		if (bother == 'y') {
+			continue;
+		}
+		if (bother == 'n') {
+			std::cout << "Thank you for visiting our shop. Good luck for you mission.\n";
+		}
+		break;
 	}
 
 	if (bought == 1) {
 		std::cout << "would you like to view your inventory? (y/n)\n";
 		std::cin >> viewInv;
 
-		for (int i = 0; i < numitems; i += 1) {
-			if (inventory[i] > 0) {
-				std::cout << inventory[i] << " x " << shopitems[i] << std::endl;
+		if (viewInv == 'y') {
+			for (int i = 0; i < numitems; i += 1) {
+				if (inventory[i] > 0) {
+					std::cout << inventory[i] << " x " << shopitems[i] << std::endl;
+				}
 			}
+		}
+		else {
+			std::cout << "Alright buddy, Good luck for your mission.\n";
 		}
 	}
 	system("PAUSE");
